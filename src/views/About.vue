ed032918a9c2ff7ed02e7fee5492ad2f810d090f
WangHyungJun <whj2013123218@gmail.com>
2019-03-12 07:42:21 +0900

    User Sign Up Test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+      <div>이메일을 입력하세요: <input v-model="loginform.email" type="text" placeholder="example@gmail.com"/></div>
+      <div>유저네임을 입력하세요: <input v-model="loginform.username" type="text" placeholder="왕형준"/></div>
+      <span v-show="!message.usernamevalid" style="color: red; font-size: 9px;">유져네임은 3글자 이상으로 지정해주세요</span>
+      <div>비밀번호를 입력하세요:<input v-model="loginform.password" type="password" placeholder="숫자 및 기호 섞어서 사용"/></div>
+      <div>비밀번호를 재입력하세요: <input v-model="loginform.checkpassword" type="password" placeholder="숫자 및 기호 섞어서 사용"/></div>
+      <button v-on:click="submit">가입하기</button>
+      <div>{{message.errormessage}}</div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        apiurl: "http://www.wanghyungjun-api.co.uk/cofounderapi/users",
+        loginform:{email: '', username: '',password: '',checkpassword: ''},
+        message: {errormessage:'', usernamevalid: false, success:''}
+      }
+    },
+    methods:{
+      submit: function () {
+          if(this.message.usernamevalid){
+              this.$http.post(this.apiurl,{
+                  email: this.loginform.email,
+                  username: this.loginform.username,
+                  password: this.loginform.password,
+                  checkpassword: this.loginform.checkpassword
+              }).then(function (data) {
+                  if(data.body['error']!=undefined) this.message.errormessage=data.body['error'][0];
+                  if(data.body['success']!=undefined) alert(data.body['success'])
+              })
+          }else{
+              this.message.errormessage="닉네임을 3글자 이상으로 정해주세요";
+          }
+      }
+    },
+    created(){
+
+    },
+    beforeUpdate(){
+        if(this.loginform.username.length>2){
+            this.message.usernamevalid=true;
+        }
+    }
+  }
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,28 @@
-const BundleTracker=require("webpack-bundle-tracker");
-
-module.exports = {
-    publicPath: "https://admiring-colden-0ad582.netlify.com/",
-    outputDir: './dist/',
-
-    chainWebpack: config => {
-
-        config.optimization
-            .splitChunks(false)
-
-        config
-            .plugin('BundleTracker')
-            .use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}])
-
-        config.resolve.alias
-            .set('__STATIC__', 'static')
-
-        config.devServer
-            .public('https://admiring-colden-0ad582.netlify.com')
-            .host('0.0.0.0')
-            .port(8080)
-            .hotOnly(true)
-            .watchOptions({poll: 1000})
-            .https(false)
-            .headers({"Access-Control-Allow-Origin": ["\*"]})
-            }
-        };
+// const BundleTracker=require("webpack-bundle-tracker");
+//
+// module.exports = {
+//     publicPath: "https://admiring-colden-0ad582.netlify.com/",
+//     outputDir: './dist/',
+//
+//     chainWebpack: config => {
+//
+//         config.optimization
+//             .splitChunks(false)
+//
+//         config
+//             .plugin('BundleTracker')
+//             .use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}])
+//
+//         config.resolve.alias
+//             .set('__STATIC__', 'static')
+//
+//         config.devServer
+//             .public('https://admiring-colden-0ad582.netlify.com')
+//             .host('0.0.0.0')
+//             .port(8080)
+//             .hotOnly(true)
+//             .watchOptions({poll: 1000})
+//             .https(false)
+//             .headers({"Access-Control-Allow-Origin": ["\*"]})
+//             }
+//         };
